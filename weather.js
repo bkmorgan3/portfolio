110fe1e7cdcf8433931b6f39e6697a524757d661
Brad Morgan <bkmorgan3@gmail.com>
2024-08-12 11:34:20 -0700

    add weather api

---
+++ weather.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const temp = document.getElementById("temp")
+    const apiKey = "1a997775e4e4cf1300a58f5cbb31b415"
+    const city = "Los Angeles"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        if(data.cod === 200) {
+            
+            temp.innerHTML = `<p> In ${city} the current temperature is ${data.main.temp}F</p>`;
+        } else {
+            console.error("Error fetching weather:", data.message)
+        }
+    })
+    .catch(err => console.error(err))
+});
